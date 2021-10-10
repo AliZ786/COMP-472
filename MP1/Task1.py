0e1b762a621024a21ce1d1c9a22411dde5470720
salehatrq <68137308+salehatrq@users.noreply.github.com>
2021-10-10 02:40:53 -0400

    Task 1.7 (a, b) Completed

--- MP1/Task1.py
+++ MP1/Task1.py
@@ -5,7 +5,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, plot_confusion_matrix
+from sklearn.metrics import confusion_matrix
+from tabulate import tabulate
 from sklearn.pipeline import make_pipeline
 
 
@@ -70,19 +71,18 @@ bbc_data = load_files('MP1/BBC',load_content=True, encoding = 'latin1')
 vectorizer = CountVectorizer()
 vectorizer.fit(bbc_data.data)
 
-vectorizer.get_feature_names_out();
+vectorizer.get_feature_names_out()
 
 #X_train
 bbc_data_transformed = vectorizer.transform(bbc_data.data)
 
 bbc_data_transformed.toarray()
 
-print()
-print(pd.DataFrame(bbc_data_transformed.toarray(), columns=vectorizer.get_feature_names_out()))
+# print(pd.DataFrame(bbc_data_transformed.toarray(), columns=vectorizer.get_feature_names_out()))
 
-print("\nSparse matrix\n" , bbc_data_transformed)
+# print("\nSparse matrix\n" , bbc_data_transformed)
 
-print ("\nDense matrix\n", bbc_data_transformed.toarray())
+# print ("\nDense matrix\n", bbc_data_transformed.toarray())
 
 
 # Task 1.5
@@ -93,22 +93,17 @@ BBC_Y = bbc_data.target
 X_train, X_test, y_train, y_test  = train_test_split(bbc_data_transformed, BBC_Y,  test_size=0.20, train_size=0.8, random_state=None)
 
 
-
 # Task 1.6 
 
 classifier_MNB = MultinomialNB()
-
 classifier_MNB.fit(X_train, y_train)
-
-pred1 = classifier_MNB.predict(X_test)
-
-print()
-
-print(type(pred1))
-
-print(pred1)
-
+prediction = classifier_MNB.predict(X_test)
 
 
 # Task 1.7
-
+f = open("bbc-performance.txt", "w")
+f.write("(a) ======= MultinomialNB default values, try 1 =======\n\n(b)\n")
+cm = confusion_matrix(y_test, prediction)
+confusion_matrix = pd.DataFrame(cm, index=x_labels)
+f.write(tabulate(confusion_matrix, x_labels))
+f.close()
