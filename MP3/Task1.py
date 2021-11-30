9984531a47700ff350b954bf09529d7abe801b3c
salehatrq <68137308+salehatrq@users.noreply.github.com>
2021-11-30 01:06:07 -0500

    Task 1.2 Completed

--- MP3/Task1.py
+++ MP3/Task1.py
@@ -5,6 +5,9 @@ def modelEvaluation(model_name):
 
     # load model
     corpus = api.load(model_name)
+
+    C = 0
+    V = len(open('MP3/synonyms.csv').readlines())-1
     
     with open('MP3/synonyms.csv', newline='') as synonyms_csv:
         reader = csv.reader(synonyms_csv, delimiter=',')
@@ -12,16 +15,32 @@ def modelEvaluation(model_name):
         next(reader)
         
         details_csv = open(model_name + '-details.csv', 'w')
-        guess_word = ''
-        label= ''
 
         for row in reader:
+            
+            guess_word = ''
+            label= ''
+
             question_word = row[0]
             correct_word = row[1]
             word_zero = row[2]
             word_one = row[3]
             word_two = row[4]
             word_three = row[5]
+                
+            # check if guess-words are found in the model
+            counter = 0
+            for j in range(2,6):
+                try:
+                    corpus[row[j]]
+                    counter += 1
+                    
+                except KeyError:
+                    print(f'The word {row[j]} was not found in this model.')
+
+            if counter == 0:
+                label = 'guess'
+                V -= 1
 
             try:
                 corpus[question_word]
@@ -45,19 +64,30 @@ def modelEvaluation(model_name):
                 elif highest_similarity == similarity_three:
                     guess_word = word_three
 
-                if guess_word == correct_word:
-                    label = 'correct'
-                else:
-                    label = 'wrong'    
+                if label != 'guess':    
+                    if guess_word == correct_word:
+                        label = 'correct'
+                        C += 1
+                    else:
+                        label = 'wrong'
 
-            except:
+            except KeyError:
                 print(f'The word {question_word} was not found in this model.')
                 label = 'guess'
-            
+                guess_word = word_one
+                V -= 1
+
             # Task 1.1
             details_csv.write(f'{question_word},{correct_word},{guess_word},{label}\n')
         
         details_csv.close()
+
+        # Task 1.2
+        analysis_csv = open('analysis.csv', 'w')
+        analysis_csv.write(f'{model_name},{len(corpus)},{C},{V},{C/V}\n')
+        analysis_csv.close()
+
+    synonyms_csv.close()    
 
 
 if __name__ == '__main__':
